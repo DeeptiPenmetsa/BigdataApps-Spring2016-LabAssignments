9406ecddbaa904b41abf83bc9b23b59f6095adce
Deepthi <deepthip1994@gmail.com>
2016-02-19 09:46:46 -0600

    lab3 committed

--- LabAssignment3/EM.R
+++
@@ -1,8 +0,0 @@
-library(mclust)
-lab3<-read.csv('lab3.csv')
-mc<-Mclust(lab3[,1:5],4)
-summary(mc)
-plot(mc,what="density",dimens=c(1,2))
-plot(mc,what="classification",dimens=c(1,2))
-table(lab3$UNS,mc$classification)
-   
--- LabAssignment3/Kmedoids.R
+++
@@ -1,7 +0,0 @@
-library(cluster)
-lab3<-read.csv('lab3.csv')
-pam.result<-pam(lab3,4)
-table(pam.result$clustering,lab3$UNS)
-layout(matrix(c(1,2),1,2))
-plot(pam.result)
-
