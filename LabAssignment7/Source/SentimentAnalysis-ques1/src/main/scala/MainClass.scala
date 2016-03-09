b3d17f864766218d1bbbfca3b1ef4dbb48f95acf
Deepthi <deepthip1994@gmail.com>
2016-03-09 07:20:00 -0600

    commit

---
+++ LabAssignment7/Source/SentimentAnalysis-ques1/src/main/scala/MainClass.scala
@@ -0,0 +1,11 @@
+/**
+ * Created by Mayanka on 23-Jul-15.
+ */
+object MainClass {
+
+  def main(args: Array[String]) {
+    val sentimentAnalyzer: SentimentAnalyzer = new SentimentAnalyzer
+    val tweetWithSentiment: TweetWithSentiment = sentimentAnalyzer.findSentiment("I hate you")
+    System.out.println(tweetWithSentiment)
+  }
+}
